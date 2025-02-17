9ce20da59cf9f5af0425e11fc5dba22fdcd10e81
bebeto040805 <vitor.albertin27@gmail.com>
2025-02-17 10:41:04 -0300

    Update EntradaDados.java

--- aula2 variaveis/src/variaveis/EntradaDados.java
+++ aula2 variaveis/src/variaveis/EntradaDados.java
@@ -2,7 +2,7 @@ package variaveis;
 
 import java.util.Scanner;
 
-public class OperadoresAritimeticos {
+public class EntradaDados {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
