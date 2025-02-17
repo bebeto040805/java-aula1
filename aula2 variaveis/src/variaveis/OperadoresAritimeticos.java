79c32438688ecf98a394510d8f4ef66d28d27caf
bebeto040805 <vitor.albertin27@gmail.com>
2025-02-17 10:38:43 -0300

    projeto

---
+++ aula2 variaveis/src/variaveis/OperadoresAritimeticos.java
@@ -0,0 +1,22 @@
+package variaveis;
+
+import java.util.Scanner;
+
+public class OperadoresAritimeticos {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		System.out.println("Digite seu nome bebeto");
+		nome = sc.next();
+		System.out.println(nome);
+		System.out.println("Digite sua idade");
+		int idade = sc.nextInt();
+		System.out.println("A sua idade é " + idade);
+		sc.close();
+		
+		
+	}
+
+}
---
+++ aula2 variaveis/src/variaveis/TiposDados.java
@@ -0,0 +1,18 @@
+package variaveis;
+
+public class TiposDados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int =
+		int idade = 40;
+		//byte= 128 - 127
+		byte numero = 127;
+		//short= 32.768 - 32.767
+		short n2 = 300;
+        long n3= 500;
+        
+        String MaiorTime = "Corinthians";
+	}
+
+}
